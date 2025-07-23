4998a98a5189ea94bdfc4f804a91440362169ede
jyothichowdary236 <jyothichowdary71@gmail.com>
2025-07-23 11:34:47 +0530

    add python files via upload

---
+++ prime.py
@@ -0,0 +1,19 @@
+import math
+
+def is_prime(num):
+    """
+    Checks if a number is prime.
+    A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+    """
+    if num <= 1:
+        return False  # Numbers less than or equal to 1 are not prime
+    
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False  # If divisible, it's not prime
+    return True  # If no divisors found, it's prime
+
+print("Prime numbers from 1 to 100 are:")
+for number in range(1, 101):
+    if is_prime(number):
+        print(number)
